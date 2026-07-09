4d036719960da584ffab0efe9437c461db1c8632
adarsh-ClushXD <adarshjaiswalofficial06@gmail.com>
2026-07-09 18:03:04 +0530

    Update flutter_project/lib/main.dart
    Co-authored-by: sougata-devlooper <sougata.dutta2007@gmail.com>

---
+++ flutter_project/lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'game_screen.dart';
+
+void main() {
+  runApp(const NumberGuesserApp());
+}
+
+class NumberGuesserApp extends StatefulWidget {
+  const NumberGuesserApp({super.key});
+
+  @override
+  State<NumberGuesserApp> createState() => _NumberGuesserAppState();
+}
+
+class _NumberGuesserAppState extends State<NumberGuesserApp> {
+  ThemeMode _themeMode = ThemeMode.light;
+
+  void toggleTheme() {
+    setState(() {
+      _themeMode = _themeMode == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Number Guessing Game',
+      debugShowCheckedModeBanner: false,
+      themeMode: _themeMode,
+      theme: ThemeData(
+        brightness: Brightness.light,
+        fontFamily: GoogleFonts.vt323().fontFamily,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        fontFamily: GoogleFonts.vt323().fontFamily,
+      ),
+      home: GameScreen(
+        onToggleTheme: toggleTheme,
+        isDark: _themeMode == ThemeMode.dark,
+      ),
+    );
+  }
+}
