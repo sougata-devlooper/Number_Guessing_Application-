29b35f6824782987031d63953e56a9c5eda256d9
adarsh-ClushXD <adarshjaiswalofficial06@gmail.com>
2026-07-18 15:49:27 +0530

    Update flutter_project/lib/game_screen.dart
    Co-authored-by: sougata-devlooper <sougata.dutta2007@gmail.com>

--- flutter_project/lib/game_screen.dart
+++ flutter_project/lib/game_screen.dart
@@ -24,6 +24,7 @@ class GameScreen extends StatefulWidget {
 class _GameScreenState extends State<GameScreen> with TickerProviderStateMixin {
   final GameState _gameState = GameState();
   final TextEditingController _guessController = TextEditingController();
+  final TextEditingController _guessController = TextEditingController();
   final FocusNode _guessFocus = FocusNode();
   
   late ConfettiController _confettiController;
